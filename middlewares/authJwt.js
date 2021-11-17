099958bfd8b2562697c70982f4ad4579ede7269c
Tomasz <tomijasinski@wp.pl>
2021-11-17 18:44:14 +0100

    delete unnecesary

--- middlewares/authJwt.js
+++ middlewares/authJwt.js
@@ -60,41 +60,9 @@ const isAdmin = (req, res, next) => {
     );
   });
 };
-/*
-const isModerator = (req, res, next) => {
-  User.findById(req.userId).exec((err, user) => {
-    if (err) {
-      res.status(500).send({ message: err });
-      return;
-    }
 
-    Role.find(
-      {
-        _id: { $in: user.roles }
-      },
-      (err, roles) => {
-        if (err) {
-          res.status(500).send({ message: err });
-          return;
-        }
-
-        for (let i = 0; i < roles.length; i++) {
-          if (roles[i].name === "moderator") {
-            next();
-            return;
-          }
-        }
-
-        res.status(403).send({ message: "Require Moderator Role!" });
-        return;
-      }
-    );
-  });
-};
-*/
 const authJwt = {
   verifyToken,
   isAdmin,
-//  isModerator
 };
 module.exports = authJwt;
